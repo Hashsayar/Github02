2f7df0b6421113b9985d9472097ac01c5eb180ff
Hashmatullah Sayar <hash.sayar76@gmail.com>
2024-11-07 01:52:22 +0100

    Second

--- src/main/java/org/example/mainjava.java
+++ src/main/java/org/example/mainjava.java
@@ -5,6 +5,9 @@ public class mainjava {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        for(int i=0; i<=100; i++) {
+            System.out.println(i);
+        }
 
     }
 }
